254a7dd5573fae7bbac40a43c89b6a62cf0bdac7
brian432 <brian_cabrera123@outlook.es>
2023-07-06 17:10:39 -0300

    expresion regular agregada para validar los inputs

--- src/utils/yup.ts
+++ src/utils/yup.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup'
 const required = 'Required field'
 
 export const personalInformationSchema = yup.object().shape({
-  fullName: yup.string().min(3, 'Ingrese mas de 2 caracteres').required(required),
-  phoneNumber: yup.string().matches(/^\d+$/, 'Enter only numbers').required(required),
+  fullName: yup.string().matches(/^[a-zA-ZÀ-ÿ]{2,}(?:\s[a-zA-ZÀ-ÿ]+)+$/, 'Enter a valid full name.').required(required),
+  phoneNumber: yup.string().min(10).matches(/^\+?\d{1,3}[-\s]?\d{3,4}[-\s]?\d{4}$/, 'Enter a valid phone number').required(required),
   contactPreference: yup.string().oneOf(["Email", "Phone"]).required(required)
 })
